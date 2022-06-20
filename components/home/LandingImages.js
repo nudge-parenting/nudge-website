0d19595d7aff22b827ca69dd67031fef96c365d0
Abhimanyu Jha <abhimanyu.in@gmail.com>
2022-06-20 16:46:25 +0530

    changed image import logic for next image

--- components/home/LandingImages.js
+++ components/home/LandingImages.js
@@ -8,7 +8,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import testPic from "../../public/landing images/landing-image-01.png";
+
+import landingImage1 from "../../public/landing images/landing-image-01.png";
+import landingImage2 from "../../public/landing images/landing-image-02.png";
+import landingImage3 from "../../public/landing images/landing-image-03.png";
+import landingImage4 from "../../public/landing images/landing-image-04.png";
+import landingImage5 from "../../public/landing images/landing-image-05.png";
+import landingImage6 from "../../public/landing images/landing-image-06.png";
+import landingImage7 from "../../public/landing images/landing-image-07.png";
+import landingImage8 from "../../public/landing images/landing-image-08.png";
+import landingImage9 from "../../public/landing images/landing-image-09.png";
+import landingImage10 from "../../public/landing images/landing-image-10.png";
+import landingImage11 from "../../public/landing images/landing-image-11.png";
+import landingImage12 from "../../public/landing images/landing-image-12.png";
+import landingImage13 from "../../public/landing images/landing-image-13.png";
+import landingImage14 from "../../public/landing images/landing-image-14.png";
+import landingImage15 from "../../public/landing images/landing-image-15.png";
 
 function LandingImages() {
 	const swiperSlideStyle = {
@@ -71,31 +86,51 @@ function ImagesCol({ imgNum, hasPt }) {
 			pt={hasPt ? { base: "8", xl: "14" } : "0"}
 		>
 			<ImageBox
-				imgPath={`/landing images/landing-image-${
-					imgNum < 10 ? "0" + imgNum : imgNum
-				}.png`}
+				// imgPath={`/landing images/landing-image-${
+				// 	imgNum < 10 ? "0" + imgNum : imgNum
+				// }.png`}
+				imgNum={imgNum}
 			/>
 			<ImageBox
-				imgPath={`/landing images/landing-image-${
-					imgNum < 10 ? "0" + (imgNum + 1) : imgNum + 1
-				}.png`}
+				// imgPath={`/landing images/landing-image-${
+				// 	imgNum < 10 ? "0" + (imgNum + 1) : imgNum + 1
+				// }.png`}
+				imgNum={imgNum + 1}
 			/>
 			<ImageBox
-				imgPath={`/landing images/landing-image-${
-					imgNum + 2 < 10 ? "0" + (imgNum + 2) : imgNum + 2
-				}.png`}
+				// imgPath={`/landing images/landing-image-${
+				// 	imgNum + 2 < 10 ? "0" + (imgNum + 2) : imgNum + 2
+				// }.png`}
+				imgNum={imgNum + 2}
 			/>
 		</VStack>
 	);
 }
 
-function ImageBox({ imgPath, ...props }) {
+function ImageBox({ imgNum, ...props }) {
+	const landingImages = [
+		landingImage1,
+		landingImage2,
+		landingImage3,
+		landingImage4,
+		landingImage5,
+		landingImage6,
+		landingImage7,
+		landingImage8,
+		landingImage9,
+		landingImage10,
+		landingImage11,
+		landingImage12,
+		landingImage13,
+		landingImage14,
+		landingImage15,
+	];
 	return (
 		<Box width={288} height={216} {...props}>
 			<Image
 				width={288}
 				height={216}
-				src={testPic}
+				src={landingImages[imgNum - 1]}
 				alt="landing image"
 				style={{
 					borderRadius: "12px",
