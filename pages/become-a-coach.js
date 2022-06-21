41545c42639e214c1838dcea7bcf337e407f633a
Abhimanyu Jha <abhimanyu.in@gmail.com>
2022-06-21 15:07:37 +0530

    hero bac

--- pages/become-a-coach.js
+++ pages/become-a-coach.js
@@ -1,23 +1,12 @@
-import { Text, Square } from "@chakra-ui/react";
-import { ScaleFade } from "@chakra-ui/react";
-const BecomeACoach = () => (
-	<ScaleFade in>
-		<Square
-			minW="50"
-			minH="90vh"
-			mt={{ base: "10", md: "110", lg: "110", xl: "140" }}
-			bg="orange.500"
-			mx={{
-				base: "8",
-				md: "90",
-				lg: "250",
-				xl: "350",
-				"2xl": "450",
-			}}
-			borderRadius="xl"
-		>
-			<Text color={"white"}>Become A Coach, coming soon.</Text>
-		</Square>
-	</ScaleFade>
-);
+import { VStack } from "@chakra-ui/react";
+import HeroSection from "../components/become a coach/HeroSection";
+
+function BecomeACoach() {
+	return (
+		<VStack spacing="48px" w="100%" pb="12">
+			<HeroSection />
+		</VStack>
+	);
+}
+
 export default BecomeACoach;
