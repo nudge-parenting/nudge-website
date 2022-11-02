eeec5e71592c62f19938616d99ae29af29b3c325
Abhimanyu Jha <abhimanyu.in@gmail.com>
2022-11-02 15:04:51 +0530

    host-path event hit test

--- pages/become-a-host.js
+++ pages/become-a-host.js
@@ -1,4 +1,5 @@
 import { VStack } from "@chakra-ui/react";
+import ReactGA from "react-ga4";
 import {
 	BacCta,
 	EarningPotentialSection,
@@ -8,7 +9,7 @@ import {
 	WhatActivitiesSection,
 	HowHostingWorks,
 } from "../components/become a host";
-
+ReactGA.send({ hitType: "pageview", page: "/host-path" });
 function BecomeACoach() {
 	return (
 		<VStack spacing="48px" w="100%" pb="12">
